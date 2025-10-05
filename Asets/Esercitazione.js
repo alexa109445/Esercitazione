a8699cdc7cf2fb3c417d13e1af53f176c09a1e8e
alexa109445 <alemanolache109@gmail.com>
2025-10-05 16:07:01 +0200

    esercitazione

---
+++ Asets/Esercitazione.js
@@ -0,0 +1,117 @@
+const bark = function () {
+  console.log("BAU");
+};
+bark();
+
+
+
+
+
+console.log("Bark x 5 volte");
+for (let i = 0; i < 5; i++) {
+  bark();
+}
+
+
+
+
+const ciaoAlexa = function () {
+  let messaggio = "Ciao Alexa";
+  messaggio = messaggio.toUpperCase();
+  console.log(messaggio);
+};
+ciaoAlexa();
+
+
+
+
+
+const Pincopallo = function () {
+  let mex = "Sono Pincopallo";
+  mex = mex.toUpperCase();
+  console.log(mex);
+};
+Pincopallo();
+
+
+
+
+
+
+const avviso = function () {
+  let avviso = "Attenzione!";
+  avviso = avviso.toUpperCase();
+  console.log(avviso);
+};
+avviso();
+
+
+
+
+
+const salutiamo = function () {
+  let mess = "Ciao ALEXA";
+  mess = mess.slice(0, 6);
+  console.log(mess);
+};
+salutiamo();
+
+
+
+
+
+const somma=function()
+{
+    let n1=5
+    let n2=10
+    let risultato=n1+n2
+    console.log("Il risultato è : " ,risultato)
+}
+somma()
+
+
+
+
+
+
+let numero1=5
+let numero2=7
+let risultato=numero1+numero2
+console.log("il risultato è: " ,risultato)
+ somma()
+
+
+
+
+ const sommiamo=function(nr1, nr2)
+ {
+ let result=nr1+nr2
+ console.log("the result is: " ,result)}
+ sommiamo(3, 6)
+ sommiamo(2, 10)
+ 
+
+ console.log("il risultato è: ", risultato)
+ 
+ console.log("CIAO MONDO")
+
+
+
+
+ const saluta=function(vai)
+ {
+console.log("ciao, ", vai)
+ }
+ saluta("viola")
+ saluta("pincopallo")
+ 
+
+ const visito=function(città)
+ {
+    console.log("io vado a",città)
+ }
+ visito("Londra")
+ visito("Parigi")
+ 
+ sommiamo("6", "7")
+ 
